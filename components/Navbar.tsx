2c33725c0c773d311554954293a61432de52a4a2
Mystic <roadtounreal2025@gmail.com>
2025-03-22 13:04:27 -0400

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,13 +8,13 @@ const Navbar: React.FC<{}> = () => {
     <div className="w-full h-[65px] bg-['#111'] fixed backdrop-blur-sm z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
         <a
-          title="ibrahim logo"
+          title="Mystic logo"
           href="/"
           className="h-auto w-auto flex flex-row items-center"
         >
           <Image
             src="/Logo.svg"
-            alt="Ibrahim Memon - Developer"
+            alt="Mystic | Editor"
             width={100}
             height={100}
             sizes="100vw"
@@ -24,7 +24,7 @@ const Navbar: React.FC<{}> = () => {
 
         <div className="flex flex-row gap-5">
           <div
-            onClick={() => window.open("mailto:ibrahimmemon930@gmail.com")}
+            onClick={() => window.open("https://discord.gg/dhvKyd7w5k")}
             className=" z-[1] bg-transparent  padding-10 cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl  text-white  py-2 px-5"
           >
             Contact
