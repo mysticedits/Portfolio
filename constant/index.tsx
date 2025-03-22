1f563c872bdfd20fce8796042986727db927bfd3
Mystic <roadtounreal2025@gmail.com>
2025-03-22 12:39:28 -0400

    Update index.tsx

--- constant/index.tsx
+++ constant/index.tsx
@@ -1,22 +1,12 @@
 export const Social_Icons = [
   {
-    link: "https://github.com/ibrahimmemonn",
-    image: "/Github.svg",
-    alt: "Ibrahim Memon - Social Media",
+    link: "https://discord.gg/dhvKyd7w5k",
+    image: "/Discord.svg",
+    alt: "Mystic Discord",
   },
   {
-    link: "https://www.instagram.com/ibii.memon/?hl=en",
-    image: "/Instagram.svg",
-    alt: "Ibrahim Memon - Social Media",
-  },
-  {
-    link: "https://www.linkedin.com/in/ibrahimmemonn/",
-    image: "/LinkedIn.svg",
-    alt: "Ibrahim Memon - Social Media",
-  },
-  {
-    link: "https://twitter.com/Ibrahimmemonnn",
-    image: "/Twitter.svg",
-    alt: "Ibrahim Memon - Social Media",
+    link: "www.youtube.com/@mysticsaetutorials",
+    image: "/Youtube.svg",
+    alt: "Mystic Youtube",
   },
 ];
